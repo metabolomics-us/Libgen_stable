ab9ec01aff5f8829c1318d9b0186fe1ca90ce001
Diego Pedrosa <linuxmant@gmail.com>
2023-07-13 15:58:09 -0700

    add .gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='Libgen',
-                 version='0.1.0',
+                 version='2.0.0',
                  description='',
                  author='Fanzhou Kong',
                  author_email='',
